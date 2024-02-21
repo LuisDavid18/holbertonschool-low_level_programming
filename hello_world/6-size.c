6d969e288517be97cdac1d0717d4f33b3b8f89aa
LuisDavid18 <luisdavidorozcocastillo@gmail.com>
2024-02-21 17:14:24 +0000

    6-size.c added

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,7 +13,7 @@ long int longintType;
 long long int longlongintType;
 float floatType;
 
-printf("size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
