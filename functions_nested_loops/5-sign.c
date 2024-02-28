6159ac6a2df41770d0980e196d9b8199afaa4f10
LuisDavid18 <luisdavidorozcocastillo@gmail.com>
2024-02-28 19:37:00 +0000

    added

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  *print_sign - funcion main
+  *
+  *@n: caracter a comparar
+  *
+  *Return: si 1 es positivo, si 0 es zero, -0 es negativo
+  *
+  */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+			return (1);
+	}
+	if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	_putchar ('-'};
+			return (-1);
+}
